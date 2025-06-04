e585896ced4bc28e905e5c7769ff4eccf90ed3b5
Isaac Milarsky <imilarsky@gmail.com>
2025-06-04 12:05:31 -0500

    alter parsing to parse by url rather than by org
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- scripts/pull-public-assets.py
+++ scripts/pull-public-assets.py
@@ -9,7 +9,7 @@ VERSION = '0.0.1'
 def main():
     
 
-    with open('codegov.json','r') as file:
+    with open('repo-manifest.json','r') as file:
         codegovDict = json.load(file)
 
     github_key = os.getenv("GITHUB_TOKEN")
@@ -36,10 +36,27 @@ def main():
             except FileExistsError:
                 print(f"File already exists: {agency_subdir}")
 
-            orgs = codegovDict[agency]["orgs"]
-            for org in orgs:
-                org = org.strip()
-                indexGen.process_organization(org,add_to_index=True,codeJSONPath=agency_subdir)
+            #orgs = codegovDict[agency]["orgs"]
+            #for org in orgs:
+            #    org = org.strip()
+            #    indexGen.process_organization(org,add_to_index=True,codeJSONPath=agency_subdir)
+
+            try:
+                for id,repo in enumerate(codegovDict[agency],1):
+                    print(f"\nChecking {repo} [{id}/{len(codegovDict[agency])}]")
+
+                    repoPath = os.path.join(agency_subdir, (repo + '.json'))
+                    repo_obj = indexGen.github.get_repo(repo.split("github.com/")[1])
+                    code_json = indexGen.save_code_json(repo_obj)
+
+                    if code_json:
+                        print(f"✅ Found code.json in {repo}")
+                        indexGen.update_index(indexGen.index,code_json,repo_obj.organization, repo_obj.name)
+                    else:
+                        print(f"❌ No code.json found in {repo}")
+            except Exception as e:
+                print(e)
+
 
             indexGen.save_index(os.path.join(AGENCY_CODEJSON_DIR, agency + "_index.json"))
             print(f"\nIndexing complete. Results saved to {AGENCY_CODEJSON_DIR}")
